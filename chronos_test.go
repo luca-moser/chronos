81376dce5573fbb30ef721296479083e086b6445
Luca Moser <moser.luca@gmail.com>
2017-04-18 12:01:47 +0200

    run tests in parallel

--- chronos_test.go
+++ chronos_test.go
@@ -11,8 +11,9 @@ func TestMain(m *testing.M) {
 }
 
 func TestOnceAtDateSchedule(t *testing.T) {
+	t.Parallel()
 	now := time.Now()
-	plan := NewOnceAtDatePlan(now.Add(time.Duration(3) * time.Second))
+	plan := NewOnceAtDatePlan(now.Add(time.Duration(2) * time.Second))
 
 	executionCount := 0
 	task := NewScheduledTask(func() {
@@ -20,14 +21,15 @@ func TestOnceAtDateSchedule(t *testing.T) {
 	}, plan)
 	defer task.Stop()
 	task.Start()
-	<-time.After(time.Duration(5) * time.Second)
+	<-time.After(time.Duration(3) * time.Second)
 	if executionCount != 1 {
 		t.Fatalf("expected execution count to be 1 but was %d", executionCount)
 	}
 }
 
 func TestOnceAfterDuration(t *testing.T) {
-	plan := NewOnceAfterDuration(time.Duration(4) * time.Second)
+	t.Parallel()
+	plan := NewOnceAfterDuration(time.Duration(2) * time.Second)
 
 	executionCount := 0
 	task := NewScheduledTask(func() {
@@ -35,16 +37,17 @@ func TestOnceAfterDuration(t *testing.T) {
 	}, plan)
 	defer task.Stop()
 	task.Start()
-	<-time.After(time.Duration(6) * time.Second)
+	<-time.After(time.Duration(3) * time.Second)
 	if executionCount != 1 {
 		t.Fatalf("expected execution count to be 1 but was %d", executionCount)
 	}
 }
 
 func TestDailySchedule(t *testing.T) {
+	t.Parallel()
 	now := time.Now()
 	plan := NewDailySchedulingPlan([]DayTime{
-		{now.Hour(), now.Minute(), now.Second() + 4},
+		{now.Hour(), now.Minute(), now.Second() + 3},
 		{now.Hour(), now.Minute(), now.Second() + 2},
 		{now.Hour(), now.Minute(), now.Second() + 1},
 	})
@@ -55,16 +58,17 @@ func TestDailySchedule(t *testing.T) {
 	}, plan)
 	defer task.Stop()
 	task.Start()
-	<-time.After(time.Duration(6) * time.Second)
+	<-time.After(time.Duration(4) * time.Second)
 	if executionCount != 3 {
 		t.Fatalf("expected execution count to be 3 but was %d", executionCount)
 	}
 }
 
 func TestWeeklySchedule(t *testing.T) {
+	t.Parallel()
 	now := time.Now()
 	plan := NewWeeklySchedulingPlan([]Weekday{
-		{day: now.Weekday(), at: DayTime{now.Hour(), now.Minute(), now.Second() + 4}},
+		{day: now.Weekday(), at: DayTime{now.Hour(), now.Minute(), now.Second() + 3}},
 		{day: now.Weekday(), at: DayTime{now.Hour(), now.Minute(), now.Second() + 2}},
 		{day: now.Weekday(), at: DayTime{now.Hour(), now.Minute(), now.Second() + 1}},
 	})
@@ -75,16 +79,17 @@ func TestWeeklySchedule(t *testing.T) {
 	}, plan)
 	defer task.Stop()
 	task.Start()
-	<-time.After(time.Duration(6) * time.Second)
+	<-time.After(time.Duration(4) * time.Second)
 	if executionCount != 3 {
 		t.Fatalf("expected execution count to be 3 but was %d", executionCount)
 	}
 }
 
 func TestMonthlySchedule(t *testing.T) {
+	t.Parallel()
 	now := time.Now()
 	plan := NewMonthlySchedulingPlan([]MonthDay{
-		{day: uint(now.Day()), at: DayTime{now.Hour(), now.Minute(), now.Second() + 4}},
+		{day: uint(now.Day()), at: DayTime{now.Hour(), now.Minute(), now.Second() + 3}},
 		{day: uint(now.Day()), at: DayTime{now.Hour(), now.Minute(), now.Second() + 2}},
 		{day: uint(now.Day()), at: DayTime{now.Hour(), now.Minute(), now.Second() + 1}},
 	})
@@ -95,7 +100,7 @@ func TestMonthlySchedule(t *testing.T) {
 	}, plan)
 	defer task.Stop()
 	task.Start()
-	<-time.After(time.Duration(6) * time.Second)
+	<-time.After(time.Duration(4) * time.Second)
 	if executionCount != 3 {
 		t.Fatalf("expected execution count to be 3 but was %d", executionCount)
 	}
